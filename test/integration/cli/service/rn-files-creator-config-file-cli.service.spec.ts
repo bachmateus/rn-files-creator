d3678124dbe582e8b08df0fc2f9d7439dd8aee18
bachmateus <mateusbrisa@gmail.com>
2022-08-30 09:28:09 -0300

    test: test react native files creator config file

---
+++ test/integration/cli/service/rn-files-creator-config-file-cli.service.spec.ts
@@ -0,0 +1,36 @@
+import { FilesManagerService } from "../../../../src/manager/service/files-manager.service";
+import { rnFilesCreatorConfigFileService } from "../../../../src/cli/cli.module";
+import { filesManagerService } from "../../../../src/manager/manager.module";
+import inquirer from 'inquirer';
+import { PromptLogger } from "../../../../src/common/logger/prompt-logger";
+
+jest.mock('../../../../src/common/logger/prompt-logger');
+const testTargetDirectory = process.cwd()+'\\test\\target-dir';
+
+describe('Create config file flow', () => {
+  beforeEach(() => {
+  })
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+  beforeAll(async() => {
+    await filesManagerService.createDirectory(testTargetDirectory)
+  })
+  afterAll(async() => {
+    const filesManagerService = new FilesManagerService();
+    if (await filesManagerService.checkIfPathExists(testTargetDirectory))
+      await filesManagerService.deleteDirectory(testTargetDirectory)
+  });
+
+  it('should SUCCESS to create a config file', async () => {
+    jest.spyOn(inquirer, 'prompt').mockResolvedValueOnce(Promise.resolve({ language: 'JavaScript', styleType: 'styled-component'}))
+    const response = await rnFilesCreatorConfigFileService.handleGetUserRnConfigFile();
+    expect(response).toEqual({"language":"JavaScript","styleType":"styled-component"})
+    expect(PromptLogger).toHaveBeenCalledWith({"interruptProcess": false, "loggerType": "CREATE", "message": "\\rn-files-config-service.json"});
+  })
+  it('should SUCCESS to get a created config file', async () => {
+    const response = await rnFilesCreatorConfigFileService.handleGetUserRnConfigFile();
+    expect(response).toEqual({"language":"JavaScript","styleType":"styled-component"})
+    expect(PromptLogger).not.toBeCalled()
+  })
+})
